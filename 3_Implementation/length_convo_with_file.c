2892305167339d4f56787529d9216bef5583740e
Sanchana-2k <ss1231@srmist.edu.in>
2021-04-14 21:52:08 +0530

    Removed unnecessary comments

--- 3_Implementation/length_convo_with_file.c
+++ 3_Implementation/length_convo_with_file.c
@@ -7,25 +7,14 @@ unit_info length_units[4] = {{"meter", "m"}, {"centimeter", "cm"}, {"foot", "'"}
 
 char length_file_name[40] = "length_convo_factor_table.txt";
 
-//void convo_func (conversion_parameter* (length_convo_i), double ip_val, int op_index_1, double *convo_buffer);
+void convertor(conversion_parameter *length_convo, int ip_index, double ip_val, int op_num, int *op_index){
+    double *convo_buffer = (double *) malloc(4*sizeof(double));
 
+    file_to_double_array (length_file_name, ip_index, convo_buffer);
 
-    void convertor(conversion_parameter *length_convo, int ip_index, double ip_val, int op_num, int *op_index){
-        double *convo_buffer = (double *) malloc(4*sizeof(double));
-
-        file_to_double_array (length_file_name, ip_index, convo_buffer);
-
-        for (int i=0; i<op_num; i++){
-            //convo_func ((length_convo+i), ip_val, op_index[i]-1, convo_buffer);
-            convo_func ((length_convo+i), ip_val, op_index[i]-1, convo_buffer, length_units);
-        }
-
+    for (int i=0; i<op_num; i++){
+        convo_func ((length_convo+i), ip_val, op_index[i]-1, convo_buffer, length_units);
     }
-    
-    /*
-    void convo_func (conversion_parameter* (length_convo_i), double ip_val, int op_index_1, double *convo_buffer){
-        length_convo_i->unit = (length_units + op_index_1);
-        length_convo_i->conversion_factor = convo_buffer[op_index_1];
-        length_convo_i->calculated_output = ip_val * length_convo_i->conversion_factor;
-    }
-    */
+
+}
+
